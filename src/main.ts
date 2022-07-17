e6042289e2cb02a7831b9cf61197cfdc973c6c17
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-07-17 14:47:45 +0300

    init: setup project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = parseInt(process.env.PORT) || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => {
+    Logger.log(`Server is running on port: ${PORT}`);
+  });
 }
 bootstrap();
