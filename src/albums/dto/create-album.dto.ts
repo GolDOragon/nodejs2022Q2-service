ed9c1782d8155c6421f5fad2576f04286e4643c1
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-07-17 20:02:11 +0300

    fix: tests

--- src/albums/dto/create-album.dto.ts
+++ src/albums/dto/create-album.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateAlbumDto {
   @IsString()
@@ -10,5 +10,6 @@ export class CreateAlbumDto {
   year: number;
 
   @IsString()
+  @IsOptional()
   artistId: string | null;
 }
